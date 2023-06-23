e9d293362a5e9f7d453728ded379119786bd6837
Samuel Armah <samuelarmah817@gmail.com>
2023-06-22 18:00:10 -0700

    stack and queue

---
+++ nop_op.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  * nop_op - Doesn’t do anything
+  * @stack: The head of the stack
+  * @line_numb: The line on which the error occurred
+  *
+  * Return: Nothing
+  */
+void nop_op(stack_t **stack, unsigned int line_numb)
+{
+	(void) stack;
+	(void) line_numb;
+}
